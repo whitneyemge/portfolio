94b0c7d47860f60fa3db65535498ff61b049f905
whitneyemge <56616945+whitneyemge@users.noreply.github.com>
2020-03-05 18:31:48 -0800

    updates

--- TGIS504/Lab5/script.js
+++ TGIS504/Lab5/script.js
@@ -1,5 +1,6 @@
 // labels along the x-axis
 var years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+
 // For drawing the lines
 var africa = [86,114,106,106,107,111,133,221,783,2478];
 var asia = [282,350,411,502,635,809,947,1402,3700,5267];
@@ -57,18 +58,22 @@ var myChart = new Chart(ctx, {
 var countries = ["China", "South Korea", "Italy", "Iran", "Japan", "France", "Spain", "United States", "Hong Kong"];
 var mort_rates = [3.9, 0.3, 3.8, 3.7, 1.9, 1.6, 0.9, 6.9, 1.9]
 
-type: 'bar',
-  data: {
-    labels: countries,
-    datasets: [
-      {
-        label: "Percent of reported cases that have been fatal",
-        backgroundColor: "#3e95cd",
-        data: mort_rates
+// Bar chart
+var ctb = document.getElementById("barChart");
+var barChart = new Chart(ctb, {
+    type: 'bar',
+    data: {
+      labels: countries,
+      datasets: [
+        {
+          label: "Percent of reported cases that have been fatal",
+          backgroundColor: "#3e95cd",
+          data: mort_rates
+        }
+      ]
+    },
+    options: {
+      legend: { display: false }
       }
-    ]
-  },
-  options: {
-    legend: { display: false },
     }
-  }
+});
