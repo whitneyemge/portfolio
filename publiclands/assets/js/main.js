218529a46ef95c3f91891b65329ad94b1b0b19af
whitneyemge <56616945+whitneyemge@users.noreply.github.com>
2020-08-11 11:53:32 -0700

    remove old folder and rename current

--- publiclands/assets/js/main.js
+++ publiclands/assets/js/main.js
@@ -1,20 +1,43 @@
 /*
-	Minimaxing by HTML5 UP
+	TXT by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var $window = $(window),
-		$body = $('body');
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
 
 	// Breakpoints.
 		breakpoints({
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
 			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			small:   [ '361px',   '736px'  ],
+			xsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			speed: 300,
+			alignment: 'center'
+		});
+
+	// Scrolly
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $nav.height() - 5; }
 		});
 
 	// Nav.
@@ -28,7 +51,7 @@
 			)
 				.appendTo($body);
 
-		// Navigation Panel.
+		// Panel.
 			$(
 				'<div id="navPanel">' +
 					'<nav>' +
